44235a5bb30206ec6942261f908b551af938aeba
5003385 <91500260+5003385@users.noreply.github.com>
2021-12-11 12:08:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string.h>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+int main() {
+
+    char a[10][20], b[20];
+
+    for (int i = 0; i < 10; ++i) {
+        cin >> a[i];
+
+
+    }
+
+    cin >> b;
+
+
+    lib(a, b);
+    posizione(a, b);
+
+
+    return 0;
 }
